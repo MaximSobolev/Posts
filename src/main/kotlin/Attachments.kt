b15a3ee6fcc628204e5618eb545ce6eacd5e27fd
MaximSobolev <optins66@gmail.com>
2022-05-29 13:28:27 +0500

    Error correction

--- src/main/kotlin/Attachments.kt
+++ src/main/kotlin/Attachments.kt
@@ -2,45 +2,88 @@ interface Attachments {
     val type : String
 }
 
-data class Photo (
+data class PhotoAttachments (
     override val type: String = "photo",
+    private val id: Int,
+    private val albumId: Int,
+    private val ownerId: Int,
+    private val userId: Int,
+    private val text : String,
+    private val date : Int,
+    val photo : Photo = Photo (id, albumId, ownerId, userId, text, date)
+        ) : Attachments
+
+data class VideoAttachments (
+    override val type: String = "video",
+    private val id: Int,
+    private val ownerId: Int,
+    private val title : String,
+    private val description : String,
+    private val date : Int,
+    val video : Video = Video (id,ownerId, title, description, date)
+) : Attachments
+
+data class AudioAttachments (
+    override val type: String = "audio",
+    private val id: Int,
+    private val ownerId: Int,
+    private val artist : String,
+    private val title : String,
+    private val date: Int,
+    val audio : Audio = Audio (id,ownerId, artist, title, date)
+) : Attachments
+
+data class DocAttachments (
+    override val type: String = "doc",
+    private val id: Int,
+    private val ownerId: Int,
+    private val title : String,
+    private val size : Int,
+    private val date: Int,
+    val doc : Doc = Doc (id,ownerId, title, size, date)
+) : Attachments
+
+data class LinkAttachments (
+    override val type: String = "link",
+    private val url: String,
+    private val title : String,
+    val link : Link = Link (url, title)
+) : Attachments
+
+data class Photo (
     val id: Int,
     val albumId: Int,
     val ownerId: Int,
     val userId: Int,
     val text : String,
     val date : Int
-) : Attachments
+)
 
 data class Video (
-    override val type: String = "video",
     val id: Int,
     val ownerId: Int,
     val title : String,
     val description : String,
     val date : Int
-) : Attachments
+)
 
 data class Audio (
-    override val type: String = "audio",
     val id: Int,
     val ownerId: Int,
     val artist : String,
     val title : String,
     val date: Int
-) : Attachments
+)
 
 data class Doc (
-    override val type: String = "doc",
     val id: Int,
     val ownerId: Int,
     val title : String,
     val size : Int,
     val date: Int
-) : Attachments
+)
 
 data class Link (
-    override val type: String = "link",
     val url: String,
     val title : String
-) : Attachments
+)
